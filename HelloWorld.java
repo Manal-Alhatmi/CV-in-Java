b734bf76539fb5fba5ee4ad3c5e23e9b9ff1c7c0
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-06-26 14:34:37 +0400

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Name: Manal Al Hatmi");
+        System.out.println("Contact:");
+        System.out.println("Email: manalalhatmi@outlook.com");
+        System.out.println("Phone: +968-97555921");
+        System.out.println("Education:");
+        System.out.println("Degree: B.Tech in Electronics and Telecommunications Engineering");
+        System.out.println("GPA: 3.26");
+        System.out.println("University: UTAS-Al Musannah");
+        System.out.println("Skills:");
+        System.out.println("skills: Python, Exel, PowerBI, Microsoft Azure, Networking, Cisco Packet Tracer, Data Analysis, HTML, CSS");
+        System.out.println("Training:");
+        System.out.println("Research Assistant/Sultan Qaboos University/TRA");
+        System.out.println("Data Analysit and Visualization With Microsoft PowerBI/Makeen Nanodegree Program");
+        System.out.println("Trainee/Petrolum Development Oman");
+        System.out.println("Trainee/Royal Navy Of Omen");
+        System.out.println("Projects:");
+        System.out.println("EMF Exposure Measurements");
+        System.out.println("Fresh and Rotten Fruit Detection Using RaspberryPi and Python");
+        System.out.println("Web Application using Azure App Service");
+        System.out.println("Creat an executive level power BI report comparing performance between two smart pet fitness devices for dog and cat");
+        System.out.println("Languages:");
+        System.out.println("Arabic, English");
+    }
+}
+
